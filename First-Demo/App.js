6125b3789e8a3fcc0b6aeb720e3d6d19f6d7b938
DhiaMbarki <tahridhia22@gmail.com>
2022-03-17 22:04:18 +0100

    first step

---
+++ First-Demo/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>let s start fucking the world by going to malaysia next Year</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '30',
+    
+  },
+  
+});
